927d5554c4596b0e72c5acdf0565b19fe9d219c3
Mikhail <pmp@nopreset.ru>
2021-09-18 03:48:24 -0400

    good animation

--- src/client/react/components/main_links/index.js
+++ src/client/react/components/main_links/index.js
@@ -25,29 +25,52 @@ class MainLinks extends Component {
             }
         ]
 
-        const variants = {
-            visible: i => ({
-              opacity: 1,
-              transition: {
-                delay: i * 0.3,
-              },
-            }),
-            hidden: { opacity: 0 },
+        const list = {
+            visible: { 
+                opacity: 1,
+                when: "beforeChildren",
+                transition: {
+                    duration: 0.2,
+                    delayChildren: 2.5,
+                    staggerChildren: 0.1,
+
+                }
+            },
+            hidden: { 
+                opacity: 0 
+            },
+          }
+          
+        const item = {
+            visible: { 
+                y: 0, 
+                opacity: 1, 
+                transition: {
+                    duration: 0.2,
+                    type: "spring",
+                    stiffness: 122,
+                    damping: 22,
+
+                } 
+            },
+            hidden: { 
+                y: 50, 
+                opacity: 0 
+            },
         }
           
         return(
             <div className = "main-links-container" >
                     <motion.ul
                         className={classNames({ "active": !(this.props.location.pathname == "/") }, "main-links")}
+                        initial="hidden"
+                        animate="visible"
+                        variants={list}
                     >
                         {links.map((link, i) => {
-                            console.log(i)
                             return (
                                 <motion.li
-                                    custom={i}
-                                    variants={variants}
-                                    initial="hidden"
-                                    animate="visible"
+                                    variants={item}
                                     key={link.url}
                                     className={classNames("main-link-container", {
                                         "main-link-active": this.isActivePath(link.url)
