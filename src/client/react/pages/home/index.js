78b633be85dbb1038427a62fb334dfb84f149d8e
Mikhail <pmp@nopreset.ru>
2021-09-18 19:18:01 -0400

    decent animation with inter

--- src/client/react/pages/home/index.js
+++ src/client/react/pages/home/index.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { withRouter, Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import classNames from "classnames"
+import { motion } from "framer-motion";
 
 class HomePage extends Component {
 
@@ -19,11 +20,92 @@ class HomePage extends Component {
 	render() {
         let pageTitle = this.props.app.site.title + " – " + this.props.app.site.subtitle + " | Home"
 
+        const heroTitle = {
+            visible: { 
+                scale: 1, 
+                transition: {
+                    duration: 1,
+                    delay: 0.3
+                } 
+            },
+            hidden: { 
+                scale: 0.99
+            },
+        }
+
+        const heroSubtitle = {
+            visible: { 
+                scale: 1, 
+                y: 0,
+                opacity: 1,
+                transition: {
+                    duration: 1,
+                    delay: 0.55
+                } 
+            },
+            hidden: { 
+                scale: 0.99,
+                opacity: 0
+            },
+        }
+
+        const heroButton = {
+            visible: { 
+                scale: 1, 
+                opacity: 1,
+                transition: {
+                    duration: 1,
+                    delay: 0.65
+                } 
+            },
+            hidden: { 
+                scale: 0.99,
+                opacity: 0
+            },
+        }
+
 		return (
      		<div className="route-content home-route">
                 {this.renderHead(pageTitle)}
 
-                <div className="placeholder"></div>
+                <div className="placeholder">
+
+                    <div 
+                        className="hero-container"
+                        style={{
+                            height: this.props.app.clientHeight-400 + "px"
+                        }}
+                    >
+
+                        <div className="hero-content-container">
+                            <motion.div 
+                                className="hero-title animation-text-wipe animate-in"
+                                initial="hidden"
+                                animate="visible"
+                                variants={heroTitle}
+                            >
+                                {this.props.app.site.subtitle}
+                            </motion.div>
+                            <motion.div 
+                                className="hero-subtitle"
+                                initial="hidden"
+                                animate="visible"
+                                variants={heroSubtitle}
+                            >
+                                {this.props.app.site.description}
+                            </motion.div>
+                            <motion.div 
+                                className="hero-button"
+                                initial="hidden"
+                                animate="visible"
+                                variants={heroButton}
+                            >
+                                Book Appointment
+                            </motion.div>
+                        </div>
+                    </div>
+
+                </div>
 			</div>
 				
 		);
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -62,6 +62,10 @@ export default (
         ${helmetInstance.title.toString()}
         ${helmetInstance.meta.toString()}
         <link rel="stylesheet" href="/${buildAssets.bundle.css}">
+        <link>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
         <meta name="viewport" content="width=device-width; height=device-height; initial-scale=1.0; maximum-scale=1.0;"/>
       </head>
       <body id="body">
